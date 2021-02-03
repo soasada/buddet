6889b87f6f4ed67eb53634edd8173ced3fdb6184
Nicolas Vargas Ortega <nvortega92@gmail.com>
2021-02-03 19:19:03 +0100

    Working in the repository layer

--- buddet_db/src/database/entity.rs
+++ buddet_db/src/database/entity.rs
@@ -2,5 +2,6 @@ use mongodb::bson::Document;
 
 pub trait Entity {
     fn collection(&self) -> &str;
-    fn convert_to_doc(&self) -> Document;
+    fn convert_to_doc<T>(entity: T) -> Document;
+    fn convert_to_entity<T>(document: Document) -> T;
 }
--- buddet_db/src/user/mod.rs
+++ buddet_db/src/user/mod.rs
@@ -1,1 +1,31 @@
-pub mod mongo_user_repository;
+use mongodb::{
+    bson::{Document, doc, Bson}
+};
+use crate::database::entity::Entity;
+use buddet_core::user::User;
+
+pub struct UserEntity;
+
+impl Entity<User> for UserEntity {
+    fn collection(&self) -> &str {
+        return "user";
+    }
+
+    fn convert_to_doc(user: User) -> Document {
+        doc! {
+          "firstname": user.firstname,
+          "lastname": user.lastname,
+          "email": user.email,
+          "password": user.password
+        }
+    }
+
+    fn convert_to_entity(document: Document) -> User {
+        User {
+            firstname: document.get("firstname").unwrap().to_string(),
+            lastname: document.get("lastname").unwrap().to_string(),
+            email: document.get("email").unwrap().to_string(),
+            password: document.get("password").unwrap().to_string(),
+        }
+    }
+}
