b35fba34bd3febc08a2ae9340c0ca51bca23a8ba
Nicolas Vargas Ortega <nvortega92@gmail.com>
2021-02-13 20:18:52 +0100

    Separating filters and handlers

--- buddet_server/src/main.rs
+++ buddet_server/src/main.rs
@@ -1,57 +1,60 @@
-use warp::{Filter, Rejection, http::Response};
 use buddet_db::database::mongo_database::MongoDatabase;
-use buddet_core::user::User;
-use crate::user::create_user_request::CreateUserRequest;
-use buddet_db::database::{save, find};
-use buddet_db::database::entity::Entity;
-use buddet_db::user::UserEntity;
 
 mod user;
 
 #[tokio::main]
 async fn main() {
-    let user = User::new("John", "Smith", "test1111@example.org", "p4ssw0rd");
-
     if let Ok(mdb) = MongoDatabase::new("mongodb://admin:password@localhost:27022/buddetdb", "buddetdb").await {
-        let result = save(&mdb, UserEntity::from(user)).await;
-        match result {
-            Ok(inserted) => {
-                if let Some(found_user) = find(&mdb, UserEntity::collection(), inserted.as_object_id().unwrap(), UserEntity::convert_to_entity).await {
-                    println!("user: {}", found_user);
-                }
-            }
-            Err(err) => println!("{}", err.to_string())
-        }
-
-        use warp::{body, get, path, path::end, post};
-
-        let POST = |desired_path: &str, func: fn(_) -> Result<Response, Rejection>| post().and(path(desired_path)).and(body::content_length_limit(1024 * 16)).and(body::json()).and_then(func);
-
-        let routes = warp::any()
-            // POST /register
-            .or(POST("register", register));
-
-
-        /*let create_user = warp::post()
-            .and(warp::path("users"))
-            .and(warp::body::content_length_limit(1024 * 16))
-            .and(warp::body::json())
-            .map(|create_user_request: CreateUserRequest| {
-                let user = User::new(create_user_request.firstname.as_str(), create_user_request.lastname.as_str(), create_user_request.email.as_str(), create_user_request.email.as_str());
-                mur.save(&mdb, user);
-                warp::reply::json(&create_user_request);
-            });*/
-
-        let hello = warp::path!("hello" / String)
-            .map(|name| format!("Hello, {}!", name));
-
-        warp::serve(hello)
+        let api = filters::new(&mdb);
+        warp::serve(api)
             .run(([127, 0, 0, 1], 3030))
             .await;
     }
 }
 
-async fn register(request: CreateUserRequest) -> Result<Response, Rejection> {
-    println!("Incoming request {}", request);
-    return Ok(Response::builder().header("asd", "asd").body(""));
+mod filters {
+    use super::handlers;
+    use buddet_db::database::mongo_database::MongoDatabase;
+    use warp::{Filter, body};
+
+    pub fn new<'a>(db: &'a MongoDatabase) -> impl Filter<Extract = impl warp::Reply + 'a, Error=warp::Rejection> + Clone {
+        register(db.clone())
+    }
+
+    pub fn register<'a>(db: &'a MongoDatabase) -> impl Filter<Extract = impl warp::Reply + 'a, Error = warp::Rejection> + Clone {
+        warp::path("register")
+            .and(warp::post())
+            .and(body::content_length_limit(1024 * 16))
+            .and(body::json())
+            .and(with_db(db))
+            .and_then(handlers::register_handler)
+    }
+
+    fn with_db<'a>(db: &'a MongoDatabase) -> impl Filter<Extract = (&'a MongoDatabase,), Error = std::convert::Infallible> + Clone {
+        warp::any().map(move || db.clone())
+    }
+}
+
+mod handlers {
+    use crate::user::create_user_request::CreateUserRequest;
+    use buddet_db::database::mongo_database::MongoDatabase;
+    use std::convert::Infallible;
+    use buddet_core::user::User;
+    use buddet_db::database::save;
+    use buddet_db::user::UserEntity;
+    use warp::http::StatusCode;
+
+    pub async fn register_handler(request: CreateUserRequest, db: &MongoDatabase) -> Result<impl warp::Reply, Infallible> {
+        let user = User::new(
+            request.firstname.as_str(),
+            request.lastname.as_str(),
+            request.email.as_str(),
+            request.password.as_str()
+        );
+        if let Ok(inserted) = save(db, UserEntity::from(user)).await {
+            Ok(StatusCode::CREATED)
+        } else {
+            Ok(StatusCode::BAD_REQUEST)
+        }
+    }
 }
